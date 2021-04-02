5a84c3d6a3a995580aa846d3aa4b2a12bdf88cd0
Ankit Upadhyay <uankit53@outlook.com>
2021-04-02 20:10:20 +0530

    Updated JavaScript code The index and styling is already done by freeCodeCamp ,  since we are practicing JavaScript , our main focus is to write logic codes .

---
+++ colorFlipper/app.js
@@ -0,0 +1,10 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let randomNumber = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
---
+++ colorFlipper/hex.js
@@ -0,0 +1,14 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let randomNumber = "#";
+  for (let i = 0; i < 6; i++) {
+    let hexNumber = Math.floor(Math.random() * hex.length);
+    randomNumber += hexNumber;
+  }
+  document.body.style.backgroundColor = hex[randomNumber];
+  color.textContent = hex[randomNumber];
+});
