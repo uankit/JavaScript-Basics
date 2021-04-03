a526a029c29ac3b2c20c3fc03e0533a17dc52d2a
Ankit Upadhyay <uankit53@outlook.com>
2021-04-03 12:20:08 +0530

    Responsive Navbar toggle

---
+++ navbar/app.js
@@ -0,0 +1,21 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  //Using add and remove function
+
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+
+  //Using toggle function
+  links.classList.toggle("show-links");
+});
